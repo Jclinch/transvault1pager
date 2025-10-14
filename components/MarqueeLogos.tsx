9275745d94870b71e1248f0998e5ef140895df5b
sunny <sunnyugwu2011@gmail.com>
2025-10-14 20:53:32 +0100

    large

--- components/MarqueeLogos.tsx
+++ components/MarqueeLogos.tsx
@@ -10,7 +10,7 @@ const PARTNERS: Partner[] = [
   { id: "p2", alt: "Partner 2", src: "/logos/logo2.png" },
   { id: "p3", alt: "Partner 3", src: "/logos/logo3.png" },
   { id: "p4", alt: "Partner 4", src: "/logos/logo4.png" },
-  { id: "p6", alt: "Partner 6", src: "/logos/logo2.png" },
+  { id: "p5", alt: "Partner 5", src: "/logos/logo2.png" },
 ];
 
 export default function MarqueeLogos() {
@@ -19,19 +19,18 @@ export default function MarqueeLogos() {
       <div className="max-w-7xl mx-auto px-6 lg:px-8">
         <div className="overflow-hidden relative">
           {/* Marquee strip */}
-          <div className="marquee flex gap-16 whitespace-nowrap will-change-transform">
-            {[...PARTNERS, ...PARTNERS].map((p, i) => (
+            <div className="marquee flex gap-8 whitespace-nowrap will-change-transform  ">
+            {[...PARTNERS, ...PARTNERS, ...PARTNERS,].map((p, i) => (
               <div
                 key={`${p.id}-${i}`}
-                className="inline-flex items-center justify-center w-[280px] h-[100px] " // increased container height and width
+                className="flex-none relative inline-flex items-center justify-center w-[200px] h-[110px] md:w-[220px] md:h-[140px] "
               >
-                {/* ✅ Increased image size and removed fixed Tailwind height restriction */}
+                {/* Use Image fill so the image always follows the container size */}
                 <Image
                   src={p.src}
                   alt={p.alt}
-                  width={200} // increased width
-                  height={100} // increased height
-                  className="object-contain opacity-90 hover:opacity-100 transition-transform duration-300 hover:scale-105" // added smooth zoom effect
+                  fill
+                  className="object-contain p-4 opacity-95 hover:opacity-100 transition-transform duration-300 hover:scale-105"
                 />
               </div>
             ))}
